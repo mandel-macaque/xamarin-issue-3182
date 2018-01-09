e378a340847537b08d4abcb2db6d55eeb7c82b9d
Manuel de la Pena <mandel@microsoft.com>
2018-01-09 13:04:27 +0100

    Add init sample code.

---
+++ Issue3182/MyViewController.cs
@@ -0,0 +1,21 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Issue3182
+{
+	public partial class MyViewController : UIViewController
+	{
+		public string LabelText { get; set; }
+		
+		public MyViewController (IntPtr handle) : base (handle)
+		{
+		}
+		
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			Label.Text = LabelText;
+		}
+	}
+}
